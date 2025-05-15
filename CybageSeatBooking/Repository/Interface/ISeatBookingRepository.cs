ba75aa6829521f213145728550d8547f6f9a0761
Hussain <hussaindu@cybage.com>
2025-05-15 11:29:53 +0530

    Repository layer is added

---
+++ CybageSeatBooking/Repository/Interface/ISeatBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using CybageSeatBooking.Models;
+
+namespace CybageSeatBooking.Repository.Interface
+{
+    public interface ISeatBookingRepository
+    {
+        Task<List<SeatBooking>> GetBookingsByEmployeeIdAsync(string employeeId);
+        Task<List<Seat>> GetAllSeatsWithBookingsAsync();
+        Task AddBookingAsync(SeatBooking booking);
+        Task<SeatBooking?> GetBookingByIdAsync(int id);
+        Task DeleteBookingAsync(SeatBooking booking);
+        Task SaveChangesAsync();
+    }
+}
